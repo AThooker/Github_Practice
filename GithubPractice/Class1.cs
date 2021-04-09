0e34821b2185c3cce25a46c8a89675ae0a8b3993
AThooker <austintrent@icloud.com>
2021-04-09 18:37:36 -0400

    conflicts

--- GithubPractice/Class1.cs
+++ GithubPractice/Class1.cs
@@ -9,7 +9,7 @@ namespace GithubPractice
     public class GitHubClass
     {
         public int ConflictID { get; set; }
-        public int NewProp { get; set; }
+        public int ConflictProp { get; set; }
         public string NotherOne { get; set; }
         public int FiveFiveFive { get; set; }
         public decimal AnotherTwo { get; set; }

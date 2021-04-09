ac5c4e5315f47ca6b3b4379b4b4a9f74dd29f80d
AThooker <austintrent@icloud.com>
2021-04-09 18:40:19 -0400

    main method for conflicts

--- GithubPractice/Class1.cs
+++ GithubPractice/Class1.cs
@@ -8,12 +8,10 @@ namespace GithubPractice
 {
     public class GitHubClass
     {
-        public int ConflictID { get; set; }
-        public int ConflictProp { get; set; }
-        public string NotherOne { get; set; }
-        public int FiveFiveFive { get; set; }
-        public decimal AnotherTwo { get; set; }
-        public GitHubClass() { }
+        public void Main()
+        {
+
+        }
 
     }
 }
